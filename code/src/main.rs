78292676b31b824b8c84012ee027503526f487d9
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2022-06-06 17:53:21 +0200

    misc small improvements

--- code/src/main.rs
+++ code/src/main.rs
@@ -16,6 +16,7 @@
 // Lint target for code documentation. This lint enforces code
 // documentation on every code item.
 #![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
 #![deny(clippy::missing_docs_in_private_items)]
 // Lint target for code documentation. When running `rustdoc`,
 // show an error when using broken links.
